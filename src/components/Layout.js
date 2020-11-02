5a925d01fb82c11e2871c38c7a1772cae82b6cd3
Niel Kenneth <nielkenneth@moustacherepublic.com>
2020-11-02 18:06:58 +0800

    Fix case issue in imports

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+import '../styles/index.scss';
+import layoutStyles from './layout.module.scss';
+
+const Layout = ({ children }) => {
+    return (
+        <div className={layoutStyles.container}>
+            <div className={layoutStyles.content}>
+                <Header />
+                {children}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Layout;
